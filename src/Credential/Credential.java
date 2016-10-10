c838812430798ac19b7f10fc8a5a4110a51bef6e
Misindexed Orange Hat <Konnoke@Kevins-MacBook-Pro.local>
2016-10-10 12:43:09 -0400

    Package Structure

---
+++ src/Credential/Credential.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Credential;
+
+/**
+ *
+ * @author Konnoke
+ */
+public abstract class Credential {
+    double credentialID;
+    double userID;
+    
+    private double getCredentialID(){
+        return credentialID;
+    }
+    
+    private double getUserID(){
+        return userID;
+    }
+    
+    
+    
+}
