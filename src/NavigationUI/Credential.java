852138bf106eb6e21b58cca2d79826d6ac652370
Kevin Baik <Konnoke@live.com>
2016-11-09 22:58:38 -0500

    Credentials & CredentialList
    Set up Credential and CredentialList to be used in Dashboard

--- src/NavigationUI/Credential.java
+++ src/NavigationUI/Credential.java
@@ -7,8 +7,48 @@ package NavigationUI;
 
 /**
  *
- * @author Hassan
+ * @author Kevin
  */
 public class Credential {
+    
+    private String Website;
+    private String Username;
+    private String Password;
+    private String LastEdited;
+    
+    public String getWebsite() {
+        return Website;
+    }
+
+    public void setWebsite(String Website) {
+        this.Website = Website;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+    
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+    public String getLastEdited() {
+        return LastEdited;
+    }
+
+    public void setLastEdited(String LastEdited) {
+        this.LastEdited = LastEdited;
+    }
+    
+    
+    
     
 }
--- src/NavigationUI/CredentialList.java
+++ src/NavigationUI/CredentialList.java
@@ -5,10 +5,46 @@
  */
 package NavigationUI;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
- * @author Hassan
+ * @author Kevin
  */
-public class CredentialList {
-    
+public class CredentialList extends AbstractTableModel {
+
+    //public ArrayList<String> usernames;
+    private List<Credential> list = new ArrayList<Credential>();
+    private String[] columnNames = {"Website/Application", "Username", "Password", "Last edit"};
+
+    public void setList(List<Credential> list) {
+        this.list = list;
+        fireTableDataChanged();
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+
+    public int getRowCount() {
+        return list.size();
+    }
+
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return list.get(rowIndex).getUsername();
+            case 1:
+                return list.get(rowIndex).getPassword();
+            default:
+                return null;
+        }
+    }
 }
